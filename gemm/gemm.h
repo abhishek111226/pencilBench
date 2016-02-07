252f0ec3dffa8addb3600d1889f7f241cb56ead3
Keyur Joshi <keyurpjoshi@gmail.com>
2016-02-07 22:05:57 +0530

    Removing old version of gemm, since it is better to have pencil code in seperate file

--- gemm/gemm.h
+++ gemm/gemm.h
@@ -2,9 +2,9 @@
 #define ROWB COLA
 #define ROWC ROWA
 #define COLC COLB
-#define ROWA 2000
-#define COLA 2000
-#define COLB 2000
+#define ROWA 2500
+#define COLA 2500
+#define COLB 2500
 #define DIV 1
 #define alpha 1
 #define beta 1
